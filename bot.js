6fdf7186bc3829a714c44dbb59d10aa08223b489
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-11-15 20:27:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
  const Discord = require('discord.js');
 const client = new Discord.Client();
 	 const adminprefix = "M";
-const devs = ['405795595450712081' , '472573894323077150' , '406921859569942538'];
-client.on('message', message => {//for dev
+const devs = ['405795595450712081' , '406921859569942538' , '431474404007084035'];
+client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
 
@@ -14,19 +14,28 @@ if (message.content.startsWith(adminprefix + 'stream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/faresgameryt");
     message.channel.sendMessage(`**تم تغير الستريمنق  ${argresult}**`)
 } else
-	
+	if (message.content.startsWith(adminprefix+ 'wh')) {
+  client.user.setActivity(argresult, {type: "WATCHING"});
+    message.channel.sendMessage(`**${argresult} تم تغير الواتشنق **`)
+	}else 
+		if (message.content.startsWith(adminprefix+ 'l')) {
+  client.user.setActivity(argresult, {type: "LISTENING"});
+message.channel.sendMessage(`**${argresult} تم تغير اللسننق **`)
+		}
+
+
+
 client.on('message', message => {//restart
     if(message.content === adminprefix + "restart") {
           if (!devs.includes(message.author.id)) return;
               message.channel.send(`⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**`);
             console.log(`⚠️ جاري اعادة تشغيل البوت... ⚠️`);
-            Rocket.destroy();s
+            client.destroy();s
             child_process.fork(__dirname + "/bot.js");
             console.log(`تم اعادة تشغيل البوت`);
         }
 
 
     });
-	    });
-
+});
  client.login(process.env.BOT_TOKEN);
