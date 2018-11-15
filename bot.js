6c9933e94ed74df7e18262a7089fcfbf40b648d8
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-11-15 20:16:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,14 +14,7 @@ if (message.content.startsWith(adminprefix + 'stream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/faresgameryt");
     message.channel.sendMessage(`**تم تغير الستريمنق  ${argresult}**`)
 } else
-	if (message.content.startsWith(adminprefix+ 'wh')) {
-  client.user.setActivity(argresult, {type: "WATCHING"});
-    message.channel.sendMessage(`**${argresult} تم تغير الواتشنق **`)
-	}else 
-		if (message.content.startsWith(adminprefix+ 'l')) {
-  client.user.setActivity(argresult, {type: "LISTENING"});
-    message.channel.sendMessage(`**${argresult} تم تغير اللسننق **`)
-		}
+	
 client.on('message', message => {//restart
     if(message.content === adminprefix + "restart") {
           if (!devs.includes(message.author.id)) return;
